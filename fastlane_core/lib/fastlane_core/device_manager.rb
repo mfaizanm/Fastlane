94f4bffa7fb48756d6604d59ecf469de03c18430
Danielle Tomlinson <dani@circleci.com>
2017-04-21 12:59:59 +0200

    Handle simulator copy fails

--- fastlane_core/lib/fastlane_core/device_manager.rb
+++ fastlane_core/lib/fastlane_core/device_manager.rb
@@ -254,8 +254,12 @@ module FastlaneCore
         logarchive_dst = Shellwords.escape(File.join(logs_destination_dir, "system_logs-#{log_identity}.logarchive"))
         FileUtils.rm_rf(logarchive_dst)
         FileUtils.mkdir_p(logs_destination_dir)
-        command = "xcrun simctl spawn #{device.udid} log collect --output #{logarchive_dst} 2>/dev/null"
-        FastlaneCore::CommandExecutor.execute(command: command, print_all: false, print_command: true)
+        command = "xcrun simctl spawn #{device.udid} log collect --output #{logarchive_dst}"
+        begin
+          FastlaneCore::CommandExecutor.execute(command: command, print_all: true, print_command: true)
+        rescue FastlaneCore::Interface::FastlaneError
+          UI.error "Failed to copy simulator logs"
+        end
       end
     end
   end
